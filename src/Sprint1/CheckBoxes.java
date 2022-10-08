b1b4956d2967cddbedf61cc011e5e75488e57edc
bradfordd-usf <bradfordd@usf.edu>
2022-10-08 08:51:18 -0400

    context alerts

--- src/Sprint1/CheckBoxes.java
+++ src/Sprint1/CheckBoxes.java
@@ -1,5 +1,45 @@
 package Sprint1;
 
-public class CheckBoxes {
+import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class CheckBoxes {
+	public String baseUrl = "https://the-internet.herokuapp.com/";
+	public WebDriver driver;
+	@Test
+	@Parameters({ "driverPath"})
+	public void verifyBrokenImages(String driverPath) throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", driverPath);
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		driver.get(baseUrl);
+		driver.findElement(By.xpath("//a[contains(text(),'Checkboxes')]")).click();
+		WebElement title = driver.findElement(By.xpath(" //h3[contains(text(),'Checkboxes')]"));
+		Assert.assertEquals(title.getText(), "Checkboxes");
+		WebElement firstCheckbox = driver.findElement(By.xpath("//body/div[2]/div[1]/div[1]/form[1]/input[1]"));
+		WebElement secondCheckbox = driver.findElement(By.xpath("//body/div[2]/div[1]/div[1]/form[1]/input[2]"));
+		Assert.assertEquals(firstCheckbox.isSelected(), false);
+		Assert.assertEquals(secondCheckbox.isSelected(), true);
+		firstCheckbox.click();
+		secondCheckbox.click();
+		Assert.assertEquals(firstCheckbox.isSelected(), true);
+		Assert.assertEquals(secondCheckbox.isSelected(), false);
+		secondCheckbox.click();
+		Assert.assertEquals(firstCheckbox.isSelected(), true);
+		Assert.assertEquals(secondCheckbox.isSelected(), true);
+		firstCheckbox.click();
+		secondCheckbox.click();
+		Assert.assertEquals(firstCheckbox.isSelected(), false);
+		Assert.assertEquals(secondCheckbox.isSelected(), false);
+		driver.close();
+	}
 }
---
+++ src/Sprint1/ContextMenu.java
@@ -0,0 +1,53 @@
+package Sprint1;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class ContextMenu {
+	public String baseUrl = "https://the-internet.herokuapp.com/";
+	public WebDriver driver;
+	@Test
+	@Parameters({ "driverPath"})
+	public void verifyAddRemoveElements(String driverPath) throws InterruptedException {
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		driver.get(baseUrl);
+		driver.findElement(By.xpath("//a[contains(text(),'Context Menu')]")).click();
+		WebElement title = driver.findElement(By.xpath("//h3[contains(text(),'Context Menu')]"));
+		WebElement ContextMenu = driver.findElement(By.xpath("//div[@id='hot-spot']"));
+		ContextMenu.click();
+		Assert.assertEquals(isAlertPresent(), false);
+		Actions action = new Actions(driver);
+		action.moveToElement(ContextMenu).perform();
+		action.contextClick().perform();
+		Assert.assertEquals(isAlertPresent(), true);
+		String alertText = driver.switchTo().alert().getText();
+		Assert.assertEquals(isAlertPresent(), isAlertPresent());
+		driver.switchTo().alert().accept();
+		Assert.assertEquals("You selected a context menu", alertText);
+		driver.close();
+	}
+	public boolean isAlertPresent() 
+	{ 
+	    try 
+	    { 
+	        driver.switchTo().alert(); 
+	        return true; 
+	    }   
+	    catch (NoAlertPresentException Ex) 
+	    { 
+	        return false; 
+	    }   
+	} 
+}
