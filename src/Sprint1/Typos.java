3242d2f9c4c2aceb84310e61672cff92e688b9e3
bradfordd <bradfordd@usf.edu>
2022-10-14 18:06:51 -0400

    finished typos class

--- src/Sprint1/Typos.java
+++ src/Sprint1/Typos.java
@@ -1,5 +1,38 @@
 package Sprint1;
 
-public class Typos {
+import java.awt.AWTException;
+import java.util.concurrent.TimeUnit;
 
-}
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.asserts.SoftAssert;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class Typos {
+	public String baseUrl = "https://the-internet.herokuapp.com/";
+	public WebDriver driver;
+	@Test
+	@Parameters({ "driverPath"})
+	public void checkForTypos(String driverPath) throws InterruptedException, AWTException {
+		System.setProperty("webdriver.chrome.driver", driverPath);
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		SoftAssert softAssert = new SoftAssert();
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		driver.get(baseUrl);
+		driver.findElement(By.xpath("//a[contains(text(),'Typos')]")).click();
+		Thread.sleep(1000);
+		WebElement titleElement = driver.findElement(By.xpath("//h3"));
+		softAssert.assertEquals("Typos", titleElement.getText());
+		System.out.println(driver.findElement(By.xpath("//p[1]")).getText());
+		softAssert.assertEquals(driver.findElement(By.xpath("//p[1]")).getText(), "This example demonstrates a typo being introduced. It does it randomly on each page load.");
+		softAssert.assertEquals(driver.findElement(By.xpath("//p[2]")).getText(), "Sometimes you'll see a typo, other times you won't.");
+		driver.close();
+		softAssert.assertAll();
+	} 
+} 
