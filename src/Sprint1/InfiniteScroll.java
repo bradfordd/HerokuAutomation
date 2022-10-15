880a58f4af9b0b17092e66aa3fa23bdc252da3e1
bradfordd <dbradford1337@gmail.com>
2022-10-15 13:29:18 -0400

    Finished endless scroll

--- src/Sprint1/InfiniteScroll.java
+++ src/Sprint1/InfiniteScroll.java
@@ -1,20 +1,19 @@
 package Sprint1;
 
 import java.awt.AWTException;
-import java.util.NoSuchElementException;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.testng.Assert;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
-public class Input {
+public class InfiniteScroll {
 	public String baseUrl = "https://the-internet.herokuapp.com/";
 	public WebDriver driver;
 	@Test
@@ -28,27 +27,28 @@ public class Input {
 			SoftAssert softAssert = new SoftAssert();
 			driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 			driver.get(baseUrl);
+			driver.findElement(By.xpath("//a[contains(text(),'Infinite Scroll')]")).click();
 			Thread.sleep(1000);
-			driver.findElement(By.xpath("//a[contains(text(),'Inputs')]")).click();
+			JavascriptExecutor js = (JavascriptExecutor) driver; 
+			js.executeScript("window.scrollBy(0, document.body.scrollHeight)");
 			Thread.sleep(1000);
-			softAssert.assertEquals(driver.findElement(By.xpath("//h3[contains(text(),'Inputs')]")).getText(), "Inputs");
-			WebElement inputBox = driver.findElement(By.xpath("//body/div[2]/div[1]/div[1]/div[1]/div[1]/input[1]"));
-			softAssert.assertEquals(inputBox.getAttribute("value"), "");
-			inputBox.sendKeys(Keys.UP);
-			softAssert.assertEquals(inputBox.getAttribute("value"), "1");
-			inputBox.sendKeys(Keys.DOWN);
-			inputBox.sendKeys(Keys.DOWN);
-			inputBox.sendKeys(Keys.DOWN);
-			softAssert.assertEquals(inputBox.getAttribute("value"), "-2");
-			inputBox.sendKeys("5");
-			softAssert.assertEquals(inputBox.getAttribute("value"), "-25");
-			Thread.sleep(10000);
+			for (int i = 0; i < 10; i++) {
+				js.executeScript("window.scrollBy(0, document.body.scrollHeight)");
+				Thread.sleep(1000);
+			}
+			
+			Thread.sleep(1000);
+			driver.findElement(By.xpath("//h3[contains(text(),'Infinite Scroll')]"));
+			driver.findElement(By.xpath("//body/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]"));
+			WebElement firstParagraph = driver.findElement(By.xpath("//body/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]"));
+			//System.out.println(firstParagraph.getText());
+			Thread.sleep(1000);
 			driver.close();
-			softAssert.assertAll();
 		}
 		catch(Exception e) {
 			driver.close();
 			Assert.fail();
 		}
+		//driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 	}
 }
