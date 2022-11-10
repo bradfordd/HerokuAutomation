22f4d1317f3896b093759039efae62aa070a49b0
bradfordd <dbradford1337@gmail.com>
2022-11-09 19:03:50 -0500

    search bar completed

--- src/AmazonTesting/AmazonItem.java
+++ src/AmazonTesting/AmazonItem.java
@@ -3,10 +3,12 @@ package AmazonTesting;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
@@ -24,6 +26,7 @@ public class AmazonItem {
 		driver.manage().deleteAllCookies();
 		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 		driver.get(baseUrl);
+		//Search Bar Testing
 		WebElement searchBar = driver.findElement(By.xpath("//input[@id='twotabsearchtextbox']"));
 		WebElement searchBarForm = driver.findElement(By.xpath("//form[@id='nav-search-bar-form']"));
 		String searchBarFormClass = searchBarForm.getAttribute("class");
@@ -36,27 +39,32 @@ public class AmazonItem {
 	
 		searchBar.click();
 		searchBarFormClass = searchBarForm.getAttribute("class");
-		try {
-			Assert.assertEquals(searchBarFormClass, "nav-searchbar nav-progressive-attribute nav-active");
-		}
-		catch (AssertionError e) {
-			System.out.print("Test Failed: Unexpected CSS class found for searchbarForm after click.\nExpected Class: nav-input nav-searchbar-attribute nav-active\nFound Class: " + searchBarFormClass);
-		}
-		
+		Assert.assertEquals(searchBarFormClass, "nav-searchbar nav-progressive-attribute nav-active", "Test Failed: Unexpected CSS class found for searchbarForm after click.\nExpected Class: nav-input nav-searchbar-attribute nav-active\nFound Class: " + searchBarFormClass);
 		searchBar.sendKeys("1:24 Diecast");
 		String searchBarText = searchBar.getAttribute("value");
-		softAssert.assertTrue("Hello".equals("hello"), "Second soft assert failed");
+		softAssert.assertTrue(searchBarText.equals("1:24 Diecast"));
+		if (!searchBarText.equals("1:24 Diecast")) {
+			System.out.print("TEST FAILED: \n Unexpected text in search bar. \n Expected: 1:24 Diecast \n Actual: " + searchBarText);
+		}
+		searchBar.sendKeys(Keys.ENTER);
+		String strUrl = driver.getCurrentUrl();
+		try {
+			Assert.assertNotEquals(strUrl, "https://www.amazon.com/ref=nav_logo");
+		}
+		catch (AssertionError e) {
+			System.out.print("Test Failed: Could not navigate to new page after sending enter key to search bar.\n");
+			return;
+		}
+		// ITEM XPATH: VERY IMPORTANT
+		////a[@class='a-link-normal s-underline-text s-underline-link-text s-link-style a-text-normal']
 		//SoftAssert.assertEquals(searchBarText, "1:24 Diecast", "Search Bar text matches text entered.");
 		
-//		String searchBarClass = searchBar.getAttribute("class");
-//		try {
-//			Assert.assertEquals(searchBarClass, "nav-input nav-progressive-attribute");
-//		}
-//		catch (AssertionError e) {
-//			System.out.print("Unexpected CSS class found for searchbar. \n Expected Class: nav-input nav-progressive-attribute \n Found Class: " + searchBarClass);
-//		}
-//		searchBar.click();
+
 		Thread.sleep(9999999);
+		softAssert.assertAll();
+	}
+	@AfterTest
+	public void testCleanUp() {
 		driver.close();
 	}
 }
